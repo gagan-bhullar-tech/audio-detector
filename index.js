3e7e1b219075ffaac3927bd9c5de884d673a0a6e
Gagan <gagandeep@emovid.com>
2024-08-25 16:15:10 -0600

    detect audio

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ module.exports = function (stream, options) {
     // initialize the audio context
     audioContext = options.audioContext || audioContext || new audioContext();
 
+    const analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    const fftBins = new Float32Array(analyser.frequencyBinCount);
+
+    const source = audioContext.createMediaStreamSource(stream);
+
+    source.connect(analyser);
+
+    function checkAudio() {
+        analyser.getFloatFrequencyData(fftBins);
+        const maxVolume = Math.max(...fftBins);
+
+        if (maxVolume > options.threshold) {
+            console.log('Audio detected');
+        } else {
+            console.log('No audio detected');
+        }
+
+        requestAnimationFrame(checkAudio);
+    }
+
+    // start checking audio
+    checkAudio();
 };
